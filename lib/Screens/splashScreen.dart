360b43b8a9a8cbdefb35085a195695359f095442
Vincent K. Kogei <42339869+mervinkogei@users.noreply.github.com>
2024-06-01 21:17:21 +0300

    feat: Added a splash screen

---
+++ lib/Screens/splashScreen.dart
@@ -0,0 +1,36 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+class Splashscreen extends StatefulWidget {
+  const Splashscreen({super.key});
+
+  @override
+  State<Splashscreen> createState() => _SplashscreenState();
+}
+
+class _SplashscreenState extends State<Splashscreen> {
+
+
+@override
+  void initState() {
+    Timer(const Duration(seconds: 3), () {
+      Navigator.of(context).pushReplacementNamed('/home');
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Image.asset(
+          'assets/delivery.png',
+          width: 250,
+          height: 250,
+        ),
+      ),
+    );
+  }
+}
