eb373a7fde0a69e0f030f57b3fbed9050ce7aa0d
Vincent K. Kogei <42339869+mervinkogei@users.noreply.github.com>
2024-06-03 13:12:10 +0300

    feat: Working on the Home screen refactoring

--- lib/Screens/home.dart
+++ lib/Screens/home.dart
@@ -13,9 +13,9 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.orangeAccent.shade400,
-        actions: [
+        actions: const [
           Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
+            padding: EdgeInsets.symmetric(horizontal: 20),
             child: Icon(Icons.notifications_active_outlined, color: Colors.white,),
           )
         ],
@@ -57,13 +57,39 @@ class _HomeScreenState extends State<HomeScreen> {
                 child: Column(
                   children: [
                     GridView.builder(
+                      // padding: EdgeInsets.all(10),
                       itemCount: 20,
                       physics: const NeverScrollableScrollPhysics(),
                       shrinkWrap: true,
                       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                       crossAxisCount: 2), itemBuilder: (context, index){
-                        return const ListTile(
-                          title: Text('Title'),
+                        return Card(
+                          child:  Column(
+                            children: [
+                              Expanded(
+                                child: ListTile(
+                                  isThreeLine: true,
+                                  contentPadding: const EdgeInsets.only(left: 0, right: 0, bottom: 0),
+                                  title: const Text('Title'),
+                                  subtitle: Column(
+                                    children: [
+                                      ClipRRect(
+                                        borderRadius: BorderRadius.circular(10),
+                                        child: Image.asset('assets/coffee.jpg', frameBuilder: (context, child, frame, wasSynchronouslyLoaded) {
+                                          return Image.asset('assets/coffee.jpg', fit: BoxFit.cover,);
+                                        },
+                                        
+                                        
+                                        ), ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                              ElevatedButton(
+                                style: ElevatedButton.styleFrom(backgroundColor: Colors.orangeAccent),
+                                onPressed: (){}, child: const Text('Add to Cart'))
+                            ],
+                          ),
                         );
                       })
                   ],
--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
       initialRoute: '/',
       routes: {
         '/':(context) => const Splashscreen(),
-        '/home':(context) => HomeScreen()
+        '/home':(context) => const HomeScreen()
       },
     );
   }
